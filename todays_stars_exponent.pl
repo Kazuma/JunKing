6148796489f74303d340fee1875cc5419c492dd4
Kazuma Muramatsu <muramatu.k@gmail.com>
2012-05-28 16:05:39 +0900

    perltidy でインデント修正

--- todays_stars_exponent.pl
+++ todays_stars_exponent.pl
@@ -6,9 +6,9 @@ use utf8;
 binmode(STDOUT, ':utf8');
 
 my $scraper = scraper {
-  process '//tr[1]/td/b', 'day', 'TEXT';
-  process '//tr[3]/td/small/b', 'index', 'TEXT';
+    process '//tr[1]/td/b',       'day',   'TEXT';
+    process '//tr[3]/td/small/b', 'index', 'TEXT';
 };
 
-my $res = $scraper->scrape( URI->new("http://weather.yahoo.co.jp/weather/jp/expo/starry/47/9110.html") );
+my $res = $scraper->scrape(URI->new("http://weather.yahoo.co.jp/weather/jp/expo/starry/47/9110.html"));
 print $res->{day}, $res->{index}, "\n";
